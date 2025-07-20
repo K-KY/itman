ad93d2926e7d607a41e9eff6553042581b8f8766
K-KY <kyuyoungk@naver.com>
2025-07-21 01:32:52 +0900

    Feat : Jpa Enable 설정

--- src/main/java/egovframework/EgovBootApplication.java
+++ src/main/java/egovframework/EgovBootApplication.java
@@ -8,11 +8,13 @@ import org.springframework.context.annotation.Import;
 
 import egovframework.com.config.EgovWebApplicationInitializer;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Slf4j
 @ServletComponentScan
 @SpringBootApplication
 @Import({EgovWebApplicationInitializer.class})
+@EnableJpaAuditing
 public class EgovBootApplication {
 	public static void main(String[] args) {
 		log.debug("##### EgovBootApplication Start #####");
