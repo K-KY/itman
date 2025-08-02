7b27b5c5db4b4d511fe7fd264b140c40aaa0cff1
K-KY <kyuyoungk@naver.com>
2025-08-02 15:40:54 +0900

    Feat : 사용자 서비스 추가
    - UserDetailService를 구현
        - Filter에서 내부적으로 사용되는 서비스

---
+++ src/main/java/egovframework/itman/user/model/service/login/UserService.java
@@ -0,0 +1,19 @@
+package egovframework.itman.user.model.service.login;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String userId) throws UsernameNotFoundException {
+        return userRepository.findByUserId(userId);
+    }
+}
