59552315c7d6d12c74c213eb0b5eb51874b30e9d
K-KY <kyuyoungk@naver.com>
2025-08-05 00:46:34 +0900

    Test : 변수명 변경 부작용

--- src/test/java/egovframework/itman/depart/model/service/interfaces/DepartServiceTest.java
+++ src/test/java/egovframework/itman/depart/model/service/interfaces/DepartServiceTest.java
@@ -20,16 +20,16 @@ class DepartServiceTest {
 
     private int testDepartSeq = 30;
     void setUp() {
-            departService.insert(DepartDto.Request.builder().departName("test" + 1).build());
+            departService.insert(DepartDto.Request.builder().name("test" + 1).build());
     }
 
     @Test
     @Transactional
     @DisplayName("데이터 삽입 성공")
     void insert() {
-        departService.insert(DepartDto.Request.builder().departName("test").build());
+        departService.insert(DepartDto.Request.builder().name("test").build());
         Page<DepartDto.Response> read = departService.read(PageRequest.of(0, 1));
-        assertThat(read.getContent().get(0).getDepartName()).isEqualTo("test");
+        assertThat(read.getContent().get(0).getName()).isEqualTo("test");
     }
 
     @Test
@@ -39,27 +39,26 @@ class DepartServiceTest {
         Page<DepartDto.Response> read = departService.read(PageRequest.of(0, 1));
         System.out.println("read.getTotalPages() = " + read.getTotalPages());
         assertThat(read.getTotalPages()).isEqualTo(1);
-        assertThat(read.getContent().get(0).getDepartName()).isEqualTo("test" + 1);
+        assertThat(read.getContent().get(0).getName()).isEqualTo("test" + 1);
     }
 
     @Test
     @Transactional
     void update() {
         //에러남 일단 보류
-//        departService.insert(DepartDto.Request.builder().departName("test" + 1).build());
+//        departService.insert(DepartDto.Request.builder().name()("test" + 1).build());
 //        Page<DepartDto.Response> read = departService.read(PageRequest.of(0, 100));
 //        assertThat(read.getContent().get(0).getDepartName()).isEqualTo("test" + 1);
-//        departService.update(DepartDto.Request.builder().departName("test").build());
+//        departService.update(DepartDto.Request.builder().name()("test").build());
 //        read = departService.read(PageRequest.of(0, 100));
 //        assertThat(read.getContent().get(0).getDepartName()).isEqualTo("test");
     }
 
     @Test
     @Transactional
-
     void delete() {
-        DepartDto.Response insert = departService.insert(DepartDto.Request.builder().departName("test" + 1).build());
-        boolean delete = departService.delete(DepartDto.Request.builder().departSeq(1).build());
+        DepartDto.Response insert = departService.insert(DepartDto.Request.builder().name("test" + 1).build());
+        boolean delete = departService.delete(DepartDto.Request.builder().seq(1).build());
         assertThat(delete).isTrue();
 
     }
