a08a485173d5b3718aa89d3892dff6ff37dfc330
K-KY <kyuyoungk@naver.com>
2025-08-11 15:56:07 +0900

    Feat : Depart 조회, 카운트 메소드 인터페이스 정의, 인터페이스 구현
    - 전체 조회, 일부 조회 메소드
    - 전체 행 카운트, 일부 카운트

--- src/main/java/egovframework/itman/depart/model/service/interfaces/DepartService.java
+++ src/main/java/egovframework/itman/depart/model/service/interfaces/DepartService.java
@@ -2,6 +2,7 @@ package egovframework.itman.depart.model.service.interfaces;
 
 import egovframework.itman.depart.dto.DepartDto;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
 public interface DepartService {
@@ -11,11 +12,13 @@ public interface DepartService {
 
     DepartDto.Response update(DepartDto.Request departDto);
 
+    Page<DepartDto.Response> readAll(PageRequest pageRequest);
+
     boolean delete(DepartDto.Request departDto);
 
-    Long countAll();
+    Long countAll(boolean del);
 
-    Long count(boolean del);
+    Long count();
 
     DepartDto.Response updateEnable(DepartDto.Request departDto);
 }
