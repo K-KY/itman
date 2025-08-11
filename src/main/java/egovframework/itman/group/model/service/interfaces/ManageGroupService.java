74121dd7708d8c1a262134b9f5beedcbe2b5ed4f
K-KY <kyuyoungk@naver.com>
2025-08-11 20:51:22 +0900

    Feat : 그룹 서비스
    - cru soft Delete 까지 구현 완료

---
+++ src/main/java/egovframework/itman/group/model/service/interfaces/ManageGroupService.java
@@ -0,0 +1,15 @@
+package egovframework.itman.group.model.service.interfaces;
+
+import egovframework.itman.group.dto.ManageGroupDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface ManageGroupService {
+
+    Page<ManageGroupDto.Response> read(UserDetails user, PageRequest pageRequest);
+
+    ManageGroupDto.Response create(UserDetails user, ManageGroupDto.Request dto);
+
+    ManageGroupDto.Response update(UserDetails user, ManageGroupDto.Request dto);
+}
