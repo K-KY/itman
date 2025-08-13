de1bd22d6926f2e0433d38d04f47f65856333ed6
K-KY <kyuyoungk@naver.com>
2025-08-13 18:14:51 +0900

    Feat : 팩토리에 엔티티 변환 메서드 추가

--- src/main/java/egovframework/itman/depart/model/entity/DepartFactory.java
+++ src/main/java/egovframework/itman/depart/model/entity/DepartFactory.java
@@ -1,8 +1,11 @@
 package egovframework.itman.depart.model.entity;
 
 import egovframework.itman.depart.dto.DepartDto;
+import egovframework.itman.group.model.entity.ManageGroup;
 import egovframework.itman.group.model.entity.ManageGroupFactory;
 import lombok.RequiredArgsConstructor;
+
+import java.util.Optional;
 
 @RequiredArgsConstructor
 public class DepartFactory {
@@ -21,6 +24,22 @@ public class DepartFactory {
     }
 
     public static Depart toEntity(DepartDto.Request dto) {
-        return Depart.from(dto);
+//        return Depart.from(dto);
+        ManageGroup compactGroup = ManageGroupFactory.toCompactEntity(dto.getGroupSeq());
+        return Depart.builder()
+                .departSeq(dto.getSeq())
+                .departName(dto.getName())
+                .description(dto.getDescription())
+                .group(compactGroup)
+                .del(Optional.ofNullable(dto.getDel()).orElse(false))
+                .enabled(Optional.ofNullable(dto.getEnabled()).orElse(true))
+                .build();
     }
+
+//    public static Depart toEntity(DepartDto.Request dto, Long groupSeq) {
+//        ManageGroup compactGroup = ManageGroupFactory.toCompactEntity(groupSeq);
+//        return new Depart(dto.getSeq(), dto.getName(), dto.getDel()
+//                , dto.getEnabled(), dto.getDescription(),
+//                compactGroup);
+//    }
 }
