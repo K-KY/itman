8c3884d2a78bb02512c883ab0489dec891f0247f
K-KY <kyuyoungk@naver.com>
2025-08-21 09:25:54 +0900

    Feat : 변수명 수정, 조회 메소드, 업데이트 메소드 추 가

--- src/main/java/egovframework/itman/state/model/service/StateServiceImpl.java
+++ src/main/java/egovframework/itman/state/model/service/StateServiceImpl.java
@@ -13,51 +13,66 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class StateServiceImpl implements StateService {
 
-    private final StateRepository categoryRepository;
+    private final StateRepository stateRepository;
 
     @Override
     @Transactional(readOnly = true)
     public Page<State> read(int page, int size, Long groupSeq, SortDto sort) {
         Pageable pageable = PageRequest.of(page, size, sort.getSorts());
-        return categoryRepository.findAllByEnabledTrueAndDelFalseAndGroup_GroupSeq(pageable, groupSeq);
+        return stateRepository.findAllByEnabledTrueAndDelFalseAndGroup_GroupSeq(pageable, groupSeq);
+    }
+
+    @Override
+    public Page<State> readAll(int page, int size, Long groupSeq, SortDto sort) {
+        Pageable pageable = PageRequest.of(page, size, sort.getSorts());
+        return stateRepository.findAllByDelFalseAndGroup_GroupSeq(pageable, groupSeq);
     }
 
     @Override
     public Optional<State> read(String name, Long groupSeq) {
-        return categoryRepository.findByStateNameAndGroup_GroupSeq(name, groupSeq);
+        return stateRepository.findByStateNameAndGroup_GroupSeq(name, groupSeq);
     }
 
     @Override
     @Transactional
-    public State create(StateDto.Request categoryDto) {
-        Optional<State> read = read(categoryDto.getStateName(), categoryDto.getGroupSeq());
-        if (read.isPresent()) {
-            throw new IllegalArgumentException("같은 이름의 분류가 존재합니다.");
+    public State create(StateDto.Request stateDto) {
+        State read = read(stateDto.getName(), stateDto.getGroupSeq()).orElse(null);
+        if (read != null && !Objects.equals(stateDto.getSeq(), read.getStateSeq())) {
+            throw new IllegalArgumentException("같은 이름의 상태가 존재합니다.");
         }
-        State category = StateFactory.toEntity(categoryDto);
-        return categoryRepository.save(category);
+        State state = StateFactory.toEntity(stateDto);
+        return stateRepository.save(state);
     }
 
     @Override
     @Transactional
-    public State update(StateDto.Request categoryDto) {
-        Optional<State> read = read(categoryDto.getStateName(), categoryDto.getGroupSeq());
-        if (read.isPresent()) {
-            throw new IllegalArgumentException("같은 이름의 분류가 존재합니다.");
+    public State update(StateDto.Request stateDto) {
+        State read = read(stateDto.getName(), stateDto.getGroupSeq()).orElse(null);
+        if (read != null && !Objects.equals(stateDto.getSeq(), read.getStateSeq())) {
+            throw new IllegalArgumentException("같은 이름의 상태가 존재합니다.");
         }
 
-        System.out.println("categoryDto = " + categoryDto);
+        State state = stateRepository.findByStateSeq(stateDto.getSeq());
+        state.change(stateDto);
+        return state;
+    }
 
+    @Override
+    public State updateEnable(StateDto.Request stateDto) {
+        State read = read(stateDto.getName(), stateDto.getGroupSeq()).orElse(null);
+        if (read == null) {
+            throw new IllegalArgumentException("데이터를 찾을 수 없습니다");
+        }
 
-        State category = categoryRepository.findByStateSeq(categoryDto.getStateSeq());
-        category.change(categoryDto);
-        return category;
+        read.changeEnable(stateDto);
+        return read;
     }
 
 }
