b4e7a44b3f6574ba9d3b122d825f6b01b25520f6
K-KY <kyuyoungk@naver.com>
2025-08-21 09:35:01 +0900

    Refactor : 비활성 게시물 조회, 토글 API 추가

--- src/main/java/egovframework/itman/state/controller/StateController.java
+++ src/main/java/egovframework/itman/state/controller/StateController.java
@@ -10,15 +10,21 @@ import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("state")
+@RequestMapping("states")
 public class StateController {
 
     private final StateService stateService;
 
     @GetMapping
     public Page<StateDto.Response> read(int page, int size, Long groupSeq, SortDto sort) {
-        return stateService.read(page, size, groupSeq, sort).map(StateFactory::toDto);
+        return stateService.read(page - 1, size, groupSeq, sort).map(StateFactory::toDto);
     }
+
+    @GetMapping("/all")
+    public Page<StateDto.Response> readAll(int page, int size, Long groupSeq, SortDto sort) {
+        return stateService.readAll(page - 1, size, groupSeq, sort).map(StateFactory::toDto);
+    }
+
 
     @PostMapping
     public StateDto.Response create(@RequestBody StateDto.Request stateDto) {
@@ -29,4 +35,10 @@ public class StateController {
     public StateDto.Response update(@RequestBody StateDto.Request stateDto) {
         return StateFactory.toDto(stateService.update(stateDto));
     }
+
+    @PatchMapping("/enable")
+    public StateDto.Response enable(@RequestBody StateDto.Request stateDto) {
+        return StateFactory.toDto(stateService.updateEnable(stateDto));
+    }
+
 }
