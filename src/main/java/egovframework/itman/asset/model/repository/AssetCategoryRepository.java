4b027b1572014afb7dce2201a9eeb1aa96be9f62
K-KY <kyuyoungk@naver.com>
2025-08-25 23:47:05 +0900

    Feat : 그룹내 장비 카테고리 조회 메서드

--- src/main/java/egovframework/itman/asset/model/repository/AssetCategoryRepository.java
+++ src/main/java/egovframework/itman/asset/model/repository/AssetCategoryRepository.java
@@ -1,7 +1,10 @@
 package egovframework.itman.asset.model.repository;
 
 import egovframework.itman.asset.model.entity.AssetCategory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AssetCategoryRepository extends JpaRepository<AssetCategory, Long> {
+    Page<AssetCategory> findAllByCategory_GroupGroupSeq(Pageable pageable, Long groupSeq);
 }
