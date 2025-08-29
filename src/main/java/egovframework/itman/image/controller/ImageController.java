87544d9a2644c896b145f727be46b6bef6c1cc06
K-KY <kyuyoungk@naver.com>
2025-08-30 00:15:16 +0900

    Feat : 이미지 url이 없는경우 예외처리

--- src/main/java/egovframework/itman/image/controller/ImageController.java
+++ src/main/java/egovframework/itman/image/controller/ImageController.java
@@ -4,6 +4,7 @@ import egovframework.itman.image.dto.ImageFileDto;
 import egovframework.itman.image.model.entity.ImageFile;
 import egovframework.itman.image.service.ImageService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -24,6 +25,9 @@ public class ImageController {
 
     @GetMapping("/{url}")
     public ResponseEntity<ImageFileDto> read(@PathVariable String url) {
+        if (url == null || url.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
         ImageFile img = service.get(url);
 
         return ResponseEntity.of(
