cf455bb1cbcdd2717b8d2b268edd4d8737e1972a
K-KY <kyuyoungk@naver.com>
2025-09-05 23:07:17 +0900

    Feat : 로그아웃 API
    - Filter로 처리하는게 권장되는 패턴이나 일단 API로 만듬

--- src/main/java/egovframework/itman/user/controller/UserController.java
+++ src/main/java/egovframework/itman/user/controller/UserController.java
@@ -3,12 +3,19 @@ package egovframework.itman.user.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Slf4j
 @RestController
@@ -24,6 +31,23 @@ public class UserController {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         }
         return ResponseEntity.ok(user);
+    }
+
+    @PostMapping("logout")
+    public ResponseEntity<Void> deleteSession(
+            HttpServletRequest request, HttpServletResponse response, Authentication auth) {
+
+        new SecurityContextLogoutHandler().logout(request, response, auth);
+
+        //쿠키 삭제
+        ResponseCookie cookie = ResponseCookie.from("JSESSIONID", "")
+                .path("/")                // 발급 시 path와 동일
+                .maxAge(0)
+                .httpOnly(true)
+                .build();
+        response.addHeader("Set-Cookie", cookie.toString());
+
+        return ResponseEntity.noContent().build(); // 204
     }
 
 }
